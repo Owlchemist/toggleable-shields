f08e1c17b243b2065a0985705670ad13f6ec7a34
Owlchemist <owlchemist@protonmail.com>
2022-11-09 04:12:07 -0800

    1.4

--- Source/Patch.cs
+++ Source/Patch.cs
@@ -8,61 +8,65 @@ namespace ToggleableShields
 {
 
 	//Allows guns to work for disabled shields
-	[HarmonyPatch(typeof(ShieldBelt), nameof(ShieldBelt.AllowVerbCast))]
+	[HarmonyPatch(typeof(CompShield), nameof(CompShield.CompAllowVerbCast))]
 	public class Patch_AllowVerbCast
 	{
-		public static void Postfix(ShieldBelt __instance, ref bool __result)
+		public static void Postfix(CompShield __instance, ref bool __result)
 		{
 			__result = __result || __instance.energy < 0f;
 		}
     }
 
 	//Makes the shield not render while toggled off
-	[HarmonyPatch(typeof(ShieldBelt), nameof(ShieldBelt.ShouldDisplay), MethodType.Getter)]
+	[HarmonyPatch(typeof(CompShield), nameof(CompShield.ShouldDisplay), MethodType.Getter)]
 	public class Patch_ShouldDisplay
 	{
-		public static bool Prefix(ShieldBelt __instance, ref bool __result)
+		public static bool Prefix(CompShield __instance, ref bool __result)
 		{
 			return __result = __instance.energy >= 0f;
 		}
     }
 
 	//Shields have ticks and each tick they try to recharge. This just sets to recharge to 0 if it's disabled (as defined by -1 energy)
-	[HarmonyPatch(typeof(ShieldBelt), nameof(ShieldBelt.EnergyGainPerTick), MethodType.Getter)]
+	[HarmonyPatch(typeof(CompShield), nameof(CompShield.CompTick))]
 	public class Patch_EnergyGainPerTick
 	{
-		public static bool Prefix(ShieldBelt __instance)
+		public static bool Prefix(CompShield __instance)
 		{
 			return __instance.energy >= 0f;
 		}
     }
 
 	//Adds our gizmo
-	[HarmonyPatch(typeof(ShieldBelt), nameof(ShieldBelt.GetWornGizmos))]
+	[HarmonyPatch(typeof(CompShield), nameof(CompShield.CompGetWornGizmosExtra))]
 	public class Patch_GetWornGizmos
 	{
-		static IEnumerable<Gizmo> Postfix(IEnumerable<Gizmo> values, ShieldBelt __instance)
+		static IEnumerable<Gizmo> Postfix(IEnumerable<Gizmo> values, CompShield __instance)
 		{
 			//Pass along all other gizmos
             foreach (var value in values) yield return value;
 
 			//Add our gizmo
-			if ((__instance.Wearer?.IsColonistPlayerControlled ?? false) && !__instance.def.HasModExtension<StaticShield>())
+			if ((__instance.PawnOwner?.IsColonistPlayerControlled ?? false) && !__instance.parent.def.HasModExtension<StaticShield>())
 			{
 				yield return new Command_Toggle
 					{
 						defaultLabel = "ToggleableShields.Icon.Toggle".Translate(),
 						defaultDesc = "ToggleableShields.Icon.Toggle.Desc".Translate(),
-						icon = __instance.def.uiIcon ?? TexCommand.ForbidOff,
+						icon = __instance.parent.def.uiIcon ?? TexCommand.ForbidOff,
 						isActive = (() => __instance.energy >= 0f ),
 						toggleAction = delegate()
 						{
 							if (__instance.energy < 0f) 
 							{
 								__instance.energy = 0;
-								__instance.ticksToReset = __instance.StartingTicksToReset;
+								__instance.ticksToReset = __instance.Props.startingTicksToReset;
 							}
-							else __instance.energy = -0.0001f;
+							else
+							{
+								__instance.Break();
+								__instance.energy = -0.0001f;
+							}
 						}
 					};
 			}
@@ -90,29 +94,47 @@ namespace ToggleableShields
 		[HarmonyPatch(typeof(JobGiver_OptimizeApparel), nameof(JobGiver_OptimizeApparel.ApparelScoreGain))]
 		public class Patch_ApparelScoreGain
 		{
-			public static bool Prefix(Pawn pawn, Apparel ap, ref float __result)
+			public static bool Prefix(Pawn pawn, Apparel ap, ref float __result, List<float> wornScoresCache)
 			{
-				if (ap is ShieldBelt && pawn.equipment.Primary != null && pawn.equipment.Primary.def.IsWeaponUsingProjectiles)
+				if (ap.TryGetComp<CompShield>() != null && pawn.equipment.Primary != null && pawn.equipment.Primary.def.IsWeaponUsingProjectiles)
 				{
-					__result = JobGiver_OptimizeApparel.ApparelScoreRaw(pawn, ap);
+					//This code is temporarily. It's just a copy-paste done fast to push as a hotfix for now. Replacing with a transpiler is probably the cleaner solution.
+					float num = JobGiver_OptimizeApparel.ApparelScoreRaw(pawn, ap);
+					List<Apparel> wornApparel = pawn.apparel.WornApparel;
+					bool flag = false;
+					for (int i = 0; i < wornApparel.Count; i++)
+					{
+						if (!ApparelUtility.CanWearTogether(wornApparel[i].def, ap.def, pawn.RaceProps.body))
+						{
+							if (!pawn.outfits.forcedHandler.AllowedToAutomaticallyDrop(wornApparel[i]) || pawn.apparel.IsLocked(wornApparel[i]))
+							{
+								__result = -1000f;
+								return false;
+							}
+							num -= wornScoresCache[i];
+							flag = true;
+						}
+					}
+					if (!flag) num *= 10f;
+					__result = num;
 					return false;
 				}
 				return true;
 			}
 		}
 
-		//If a gun user equipa a shield belt while they're using a gun, the belt will default to off
+		//If a gun user equips a shield belt while they're using a gun, the belt will default to off
 		[HarmonyPatch(typeof(Pawn_ApparelTracker), nameof(Pawn_ApparelTracker.Wear))]
 		public class Patch_Wear
 		{
 			public static void Prefix(Apparel newApparel, Pawn_ApparelTracker __instance)
 			{
-				ShieldBelt shieldBelt = newApparel as ShieldBelt;
+				CompShield shieldBelt = newApparel.TryGetComp<CompShield>();
 				if
 				(
 					shieldBelt != null && //Is a shield belt?
 					(__instance.pawn?.equipment?.Primary?.def.IsWeaponUsingProjectiles ?? false) && //Is using a gun?
-					!shieldBelt.def.HasModExtension<StaticShield>() && //Is not a static shield?
+					!shieldBelt.parent.def.HasModExtension<StaticShield>() && //Is not a static shield?
 					__instance.pawn.Spawned //Is the pawn actually spawned?
 				)
 				{
